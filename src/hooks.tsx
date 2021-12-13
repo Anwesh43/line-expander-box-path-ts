96096a9310c4f849e410cf25f457894f333519f0
Anwesh43 <anweshthecool0@gmail.com>
2021-12-13 19:23:37 +0530

    adding useStyle hook

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -49,3 +49,45 @@ export const useDimension = () => {
 const maxScale = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
 
 const divideScale = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
+const sinify = (scale : number) : number => Math.sin(scale * Math.pI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const size : number = Math.min(w, h) / 10
+    const sf : number = sinify(scale)
+    const sc1 : number = divideScale(sf, 0, 2)
+    const sc2 : number = divideScale(sf, 1, 2)
+    const position = 'absolute'
+    const background : string = "indigo"
+    return {
+        boxStyle() {
+            const width : string = `${size}px`
+            const height : string = `${size}px`
+            const left : string = `${w / 2 - size / 2 + (w / 2 - size / 2)  * sc2}px`
+            const top = `${h / 2 - size / 2}px`
+            return {
+                position,
+                width, 
+                left, 
+                height, 
+                top,
+                background 
+            }
+        },
+        lineStyle() {
+            const lw = Math.min(w, h) / 90
+            const width = `${(w / 2 - size / 2) * sc1}px`
+            const height = `${lw}px`
+            const left = `${w / 2 + size / 2}px`
+            const top = `${h / 2 - lw / 2}px`
+            return {
+                width, 
+                height, 
+                left, 
+                top,
+                background, 
+                position  
+            }
+        }
+    }
+}
